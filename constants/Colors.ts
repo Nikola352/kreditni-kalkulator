fa5a589802cbf4e5a2e6387b6099de98a6f93c2e
Nikola Jolovic <nikolajolovic02@gmail.com>
2025-02-06 16:27:53 +0100

    Add color palette

---
+++ constants/Colors.ts
@@ -0,0 +1,47 @@
+const tintColorLight = "#c3ec4e";
+const tintColorDark = "#abdb23";
+
+export const Colors = {
+  light: {
+    text: "#577099",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#111",
+    tabIconDefault: "#111",
+    tabIconSelected: tintColorLight,
+    gradient: {
+      start: "#111",
+      middle: "#577099",
+      end: "#000",
+    },
+    primary: "#c3ec4e",
+    secondary: "#789f0b",
+    accent: "#d48624",
+    headerBackground: "#abdb23",
+    headerText: "#577099",
+    inputBorder: "#ccc",
+    tableHover: "#447",
+    tableBorder: "#57780d",
+  },
+  dark: {
+    text: "#abdb23",
+    background: "#111",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+    gradient: {
+      start: "#000",
+      middle: "#577099",
+      end: "#111",
+    },
+    primary: "#789f0b",
+    secondary: "#c3ec4e",
+    accent: "#d48624",
+    headerBackground: "#abdb23",
+    headerText: "#577099",
+    inputBorder: "#333",
+    tableHover: "#447",
+    tableBorder: "#57780d",
+  },
+};
